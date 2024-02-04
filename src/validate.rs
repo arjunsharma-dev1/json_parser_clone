dc9d54ce9247ead154722756e1c873ca6fed6dc3
Arjun Sharma <arjunsharma.dev1@gmail.com>
2024-02-05 01:51:38 +0530

    added graph DS for json validation instead of Self Managed using Rc & RefCell

--- src/validate.rs
+++ src/validate.rs
@@ -1,33 +1,36 @@
+use std::any::Any;
 use std::collections::{HashMap};
 use std::rc::Rc;
 use crate::{Token, Value};
-use std::borrow::BorrowMut;
 use std::cell::RefCell;
 use std::ops::Deref;
-use std::ops::DerefMut;
+use petgraph::Graph;
+use petgraph::graph::NodeIndex;
+use petgraph::visit::{NodeIndexable, NodeRef};
 
 pub struct JsonValidator {
+    graph: Graph<Token, Option<()>>,
     stack: Vec<Rc<Token>>,
-    node: Rc<RefCell<Node>>
+    current_node: Rc<NodeIndex>,
 }
 
-#[derive(Debug, Clone)]
+/*#[derive(Debug, Clone)]
 struct Node {
-    current_token: Rc<Token>,
-    next_tokens: HashMap<Rc<Token>, Rc<RefCell<Node>>>
-}
+    graph: Graph<Token, None>,
+    current_node: NodeIndex
+}*/
 
-impl Node {
+/*impl Node {
     fn add_multiple(&mut self, next_nodes: Vec<Rc<RefCell<Node>>>) {
         for next_node in next_nodes {
             self.next_tokens.insert(Rc::clone(&RefCell::clone(&next_node).borrow().current_token), next_node);
         }
     }
-}
+}*/
 
-impl Node {
+/*impl Node {
     fn default() -> Self {
-        let string_token = Rc::new(Token::Value(Value::String("".to_string())));
+        /*let string_token = Rc::new(Token::Value(Value::String("".to_string())));
         let number_token = Rc::new(Token::Value(Value::NumberFloating(0)));
         let boolean_token = Rc::new(Token::Value(Value::Boolean(false)));
         let null_token = Rc::new(Token::Null);
@@ -36,9 +39,9 @@ impl Node {
         let object_start_token = Rc::new(Token::ObjectStart);
         let object_end_token = Rc::new(Token::ObjectEnd);
         let comma_token = Rc::new(Token::Comma);
-        let colon_token = Rc::new(Token::Colon);
+        let colon_token = Rc::new(Token::Colon);*/
 
-        let mut value_string = Rc::new(RefCell::new(Node::new(string_token.clone())));
+        /*let mut value_string = Rc::new(RefCell::new(Node::new(string_token.clone())));
         let mut value_number = Rc::new(RefCell::new(Node::new(number_token)));
         let mut value_boolean = Rc::new(RefCell::new(Node::new(boolean_token)));
         let mut value_null = Rc::new(RefCell::new(Node::new(null_token)));
@@ -48,17 +51,19 @@ impl Node {
         let mut object_end = Rc::new(RefCell::new(Node::new(object_end_token)));
         let mut comma = Rc::new(RefCell::new(Node::new(comma_token)));
         let mut colon = Rc::new(RefCell::new(Node::new(colon_token)));
-        let mut key = Rc::new(RefCell::new(Node::new(string_token.clone())));
+        let mut key = Rc::new(RefCell::new(Node::new(string_token.clone())));*/
 
-        array_start.deref().borrow_mut().add_multiple(vec![
+
+
+        /*array_start.deref().borrow_mut().add_multiple(vec![
             Rc::clone(&object_start),
             Rc::clone(&value_boolean),
             Rc::clone(&value_null),
             Rc::clone(&value_string),
             Rc::clone(&value_number)
-        ]);
+        ]);*/
 
-        comma.deref().borrow_mut().add_multiple(vec![
+        /*comma.deref().borrow_mut().add_multiple(vec![
             Rc::clone(&value_boolean),
             Rc::clone(&value_null),
             Rc::clone(&value_string),
@@ -66,44 +71,43 @@ impl Node {
             Rc::clone(&array_start),
             Rc::clone(&object_start),
             Rc::clone(&key)
-        ]);
+        ]);*/
 
 
-        key.deref().borrow_mut().add_multiple(vec![Rc::clone(&colon)]);
-        array_end.deref().borrow_mut().add_multiple(vec![Rc::clone(&comma), Rc::clone(&object_end)]);
-        // object_start.deref().borrow_mut().add_multiple(vec![Rc::clone(&key), Rc::clone(&object_end)]);
-        object_start.deref().borrow_mut().add_multiple(vec![
+        /*key.deref().borrow_mut().add_multiple(vec![Rc::clone(&colon_node)]);*/
+        /*array_end.deref().borrow_mut().add_multiple(vec![Rc::clone(&comma), Rc::clone(&object_end)]);*/
+        /*object_start.deref().borrow_mut().add_multiple(vec![
             Rc::clone(&key),
             Rc::clone(&object_end),
             Rc::clone(&object_start) // Add this line
-        ]);
+        ]);*/
 
-        value_string.deref().borrow_mut().add_multiple(vec![Rc::clone(&comma), Rc::clone(&array_end), Rc::clone(&object_end)]);
-        value_number.deref().borrow_mut().add_multiple(vec![Rc::clone(&comma), Rc::clone(&array_end), Rc::clone(&object_end)]);
-        value_null.deref().borrow_mut().add_multiple(vec![Rc::clone(&comma), Rc::clone(&array_end), Rc::clone(&object_end)]);
-        value_boolean.deref().borrow_mut().add_multiple(vec![Rc::clone(&comma), Rc::clone(&array_end), Rc::clone(&object_end)]);
+        /*value_string.deref().borrow_mut().add_multiple(vec![Rc::clone(&comma), Rc::clone(&array_end), Rc::clone(&object_end)]);*/
+        /*value_number.deref().borrow_mut().add_multiple(vec![Rc::clone(&comma), Rc::clone(&array_end), Rc::clone(&object_end)]);*/
+        /*value_null.deref().borrow_mut().add_multiple(vec![Rc::clone(&comma), Rc::clone(&array_end), Rc::clone(&object_end)]);*/
+        /*value_boolean.deref().borrow_mut().add_multiple(vec![Rc::clone(&comma), Rc::clone(&array_end), Rc::clone(&object_end)]);*/
 
-        object_end.deref().borrow_mut().add_multiple(vec![Rc::clone(&comma), Rc::clone(&array_end), ]);
+        /*object_end.deref().borrow_mut().add_multiple(vec![Rc::clone(&comma), Rc::clone(&array_end), ]);*/
 
 
-        colon.deref().borrow_mut().add_multiple(vec![
+        /*colon_node.deref().borrow_mut().add_multiple(vec![
             Rc::clone(&value_boolean),
             Rc::clone(&value_number),
             Rc::clone(&value_null),
             Rc::clone(&value_string),
             Rc::clone(&array_start),
             Rc::clone(&object_start),
-        ]);
+        ]);*/
 
 
-        let mut root: HashMap<Rc<Token>, Rc<RefCell<Node>>> = HashMap::new();
+        /*let mut root: HashMap<Rc<Token>, Rc<RefCell<Node>>> = HashMap::new();
         root.insert(Rc::clone(&object_start_token), Rc::clone(&object_start));
         root.insert(Rc::clone(&array_start_token), Rc::clone(&array_start));
 
-        Node::new_with_next_nodes(Token::Root, root)
+        Node::new_with_next_nodes(Token::Root, root)*/
     }
 
-    fn new(token: Rc<Token>) -> Self {
+    /*fn new(token: Rc<Token>) -> Self {
         Node {
             current_token: token,
             next_tokens: HashMap::new()
@@ -114,41 +118,114 @@ impl Node {
             current_token: Rc::new(token),
             next_tokens: next_nodes
         }
-    }
-}
+    }*/
+}*/
 
 
 
 impl JsonValidator {
 
     pub fn new() -> Self {
+        let mut graph: Graph<Token, Option<()>> = Graph::new();
+        let root_node = graph.add_node(Token::Root);
+        let string_node = graph.add_node(Token::Value(Value::String("".to_string())));
+        let number_node = graph.add_node(Token::Value(Value::NumberFloating(0)));
+        let boolean_node = graph.add_node(Token::Value(Value::Boolean(false)));
+        let null_node = graph.add_node(Token::Null);
+        let array_start_node = graph.add_node(Token::ArrayStart);
+        let array_end_node = graph.add_node(Token::ArrayEnd);
+        let object_start_node = graph.add_node(Token::ObjectStart);
+        let object_end_node = graph.add_node(Token::ObjectEnd);
+        let comma_node = graph.add_node(Token::Comma);
+        let colon_node = graph.add_node(Token::Colon);
+        let key_node = graph.add_node(Token::Value(Value::String("".to_string())));
+
+        graph.add_edge(array_start_node, object_start_node, None);
+        graph.add_edge(array_start_node, boolean_node, None);
+        graph.add_edge(array_start_node, null_node, None);
+        graph.add_edge(array_start_node, string_node, None);
+        graph.add_edge(array_start_node, number_node, None);
+
+        graph.add_edge(comma_node, object_start_node, None);
+        graph.add_edge(comma_node, array_start_node, None);
+        graph.add_edge(comma_node, key_node, None);
+        graph.add_edge(comma_node, boolean_node, None);
+        graph.add_edge(comma_node, null_node, None);
+        graph.add_edge(comma_node, string_node, None);
+        graph.add_edge(comma_node, number_node, None);
+
+        graph.add_edge(key_node, colon_node, None);
+
+        graph.add_edge(object_start_node, key_node, None);
+        graph.add_edge(object_start_node, object_end_node, None);
+        graph.add_edge(object_start_node, object_start_node, None);
+
+        graph.add_edge(string_node, comma_node, None);
+        graph.add_edge(string_node, array_end_node, None);
+        graph.add_edge(string_node, object_end_node, None);
+
+        graph.add_edge(number_node, comma_node, None);
+        graph.add_edge(number_node, array_end_node, None);
+        graph.add_edge(number_node, object_end_node, None);
+
+        graph.add_edge(null_node, comma_node, None);
+        graph.add_edge(null_node, array_end_node, None);
+        graph.add_edge(null_node, object_end_node, None);
+
+        graph.add_edge(boolean_node, comma_node, None);
+        graph.add_edge(boolean_node, array_end_node, None);
+        graph.add_edge(boolean_node, object_end_node, None);
+
+        graph.add_edge(object_end_node, comma_node, None);
+        graph.add_edge(object_end_node, array_end_node, None);
+
+        graph.add_edge(colon_node, object_start_node, None);
+        graph.add_edge(colon_node, array_start_node, None);
+        graph.add_edge(colon_node, string_node, None);
+        graph.add_edge(colon_node, boolean_node, None);
+        graph.add_edge(colon_node, number_node, None);
+        graph.add_edge(colon_node, null_node, None);
+
+        graph.add_edge(root_node, object_start_node, None);
+        graph.add_edge(root_node, array_start_node, None);
+
+        graph.add_edge(array_end_node, comma_node, None);
+
+
         JsonValidator {
             stack: Vec::new(),
-            node: Rc::new(RefCell::new(Node::default()))
+            graph,
+            current_node: Rc::new(root_node),
         }
     }
+
+    fn is_neighbour(&self, first: NodeIndex, second: NodeIndex) -> bool {
+        let graph = &self.graph;
+        graph.neighbors(first).any(|neighbour| second.weight().eq(&neighbour.weight()))
+    }
+
     pub fn validate(&mut self, next_token: &Token) -> bool {
-        // dbg!(&self.node.borrow().current_token);
-        // // dbg!(&self.node.borrow().next_tokens.get(next_token).and_then(|value| Some(&value.borrow().current_token)));
-        // dbg!(next_token);
-        let previous_node = Rc::clone(&self.node);
-        let borrow = previous_node.borrow();
-        let next_node = borrow.next_tokens.get(next_token);
+        let graph: &Graph<Token, Option<()>> = &self.graph;
 
-        if next_node.is_none() {
-            return false;
+        let current_token_node = *self.current_node.deref();
+        let next_token_node = graph.raw_nodes()
+            .iter()
+            .position(|x| x.weight.eq(next_token))
+            .map(NodeIndex::new);
+        if next_token_node.is_none() {
+            panic!("Invalid JSON");
         }
-        let next_node_value = next_node.unwrap();
+        let next_token_node = next_token_node.unwrap();
+        let is_neighbour = self.is_neighbour(current_token_node, next_token_node);
 
-        let next_token = &next_node_value.deref().borrow().current_token;
-        if next_token.deref().eq(&Token::ObjectStart) || next_token.deref().eq(&Token::ArrayStart) {
-            self.stack.push(Rc::clone(next_token));
+        if next_token.eq(&Token::ObjectStart) || next_token.eq(&Token::ArrayStart) {
+            self.stack.push(Rc::new(next_token.clone()));
         }
 
-        if next_token.deref().eq(&Token::ObjectEnd) || next_token.deref().eq(&Token::ArrayEnd) {
+        if next_token.eq(&Token::ObjectEnd) || next_token.eq(&Token::ArrayEnd) {
             let is_start_present = match self.stack.last() {
-                Some(mut token) => {
-                    let end_token = get_end(token.deref()).unwrap();
+                Some(token) => {
+                    let end_token = get_end(token).unwrap();
                     next_token.deref().eq(&end_token)
                 },
                 None => false
@@ -160,8 +237,8 @@ impl JsonValidator {
             self.stack.pop();
         }
 
-        self.node = Rc::clone(next_node_value);
-        return true;
+        self.current_node = Rc::new(next_token_node);
+        is_neighbour
     }
 }
 
